dc554df3a1702e3bd009c00d85a57804155b2dab
mkm177 <mkm177@gmail.com>
2020-03-23 13:48:37 -0700

    Changes made so far

---
+++ server/resolvers.js
@@ -0,0 +1,12 @@
+const db = require('./db');
+
+const Query = {
+	job: (root, { id }) => db.jobs.get(id),
+	jobs: () => db.jobs.list()
+};
+
+const Job = {
+	company: job => db.companies.get(job.companyId)
+};
+
+module.exports = { Query, Job };
